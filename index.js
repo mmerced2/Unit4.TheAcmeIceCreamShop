b5eb1e0a38a9fc22457ee0f8d5191125220ba373
Maria Mercedes <mariamercedes@Marias-Air.lan>
2024-07-11 21:56:55 -0400

    second commit

---
+++ index.js
@@ -0,0 +1,110 @@
+//import packages needed
+const pg = require("pg");
+const express = require("express");
+//create client connection to the database
+const client = new pg.Client(
+  process.env.DATABASE_URL || "postgres://localhost:5432/postgres/the_acme_ice_cream_db"
+);
+//create the express server
+const server = express();
+
+//function to create our database table, seed data into the tables when first starting the server
+const init = async () => {
+//wait for the client to connect to the database
+    await client.connect();
+    console.log('connected to database');
+
+//create SQL to wipe the database and create a new table based on our schema
+    let SQL = `DROP TABLE IF EXISTS flavors;
+    CREATE TABLE flavors(
+        id SERIAL PRIMARY KEY, 
+        flavor VARCHAR(255), 
+        quantity INTEGER DEFAULT 3 NOT NULL, 
+        created_at TIMESTAMP DEFAULT now(),
+        updated_at TIMESTAMP DEFAULT now();
+    )`;
+//wait for the database to process the query
+    await client.query(SQL);
+    console.log('table created');
+
+//create SQL statement to insert 3 new rows of data into our table
+    SQL = `INSERT INTO notes(flavor, quantity) VALUES ('strawberry', 12);
+    INSERT INTO notes(flavor, quantity) VALUES ('vanilla', 15);
+    INSERT INTO notes(flavor, quantity) VALUES ('chocolate chip', 14);`;
+//wait for the database to process the query
+    await client.query(SQL);
+    console.log('table seeded');
+
+//have the server listen on a port
+    const port = process.env.PORT || 3000;
+    server.listen(port, () => console.log(`listening on port ${port}`));
+
+};
+
+
+//call the function so the server can start
+init();
+
+//middleware to use before all routes
+server.use(express.json()); //parses the request body so our route can access it
+server.use(require("morgan")("dev")); //logs the requests received to the server
+
+//routes
+//returns an array of note objects
+server.get("api/flavors", async (req, res, next) => {
+    try{
+        //create the SQL query to select all the notes in descending order based on when they were created
+        const SQL = `SELECT * from flavors ORDER By created_at DESC`;
+        //await the response from the client querying the database
+        const response = await client.query(SQL);
+        //send the response. If no status code is given express will send 200 by default
+        res.send(response.rows);
+    }
+    catch (error) {
+next(error);
+    }
+});
+
+
+// server.get("api/flavors/:id", (req, res, next) => {
+//     try{
+//         //create the SQL query to select all the notes in descending order based on when they were created
+//         const SQL = `SELECT * from flavors ORDER By created_at DESC`;
+//         //await the response from the client querying the database
+//         const response = await.client(SQL);
+//         //send the response. If no status code is given express will send 200 by default
+//         res.send(response.rows);
+//     }
+//     catch (error) {
+// next(error);
+//     }
+// });
+
+//adds a new note to the table
+server.post("api/flavors", async (req, res, next) => {
+    try{
+        //destructure the keys needed from the request body
+        const {flavors, quantity} = req.body;
+
+        //create the SQL query to create a new note based on the information in the request body
+        const SQL = `INSERT into flavors (flavors, quantity) VALUES($1, $2) ORDER By created_at DESC`;
+
+        //await the response from the client querying the database
+        const response = await client.query(SQL, [flavors,quantity]);
+
+        //send the response. If no status code is given express will send 200 by default
+        res.status(201).send(response.rows[0]);
+    }
+    catch (error) {
+next(error);
+    }
+});
+
+//edits a note based on the id passed and information within the request body
+server.put("api/notes/:id", async (req, res, next) => {});
+
+
+
+//deletes a note based on the id given
+server.delete("api/notes/:id", (req, res, next) => {});
+
